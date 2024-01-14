4e2cf20075b2fc1edd21aaeac4e356ac8001c642
gramaphenia <dicoratojuanpablo@gmail.com>
2024-01-14 12:16:55 -0300

    optimizacion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,51 @@
 import Image from "next/image";
 
 const Footer = () => {
+  const navegacion = [
+    {
+      titulo: "Desarrollo Web",
+    },
+    {
+      titulo: "Diseño Grafico",
+    },
+    {
+      titulo: "Modelado 3D",
+    },
+    {
+      titulo: "Difucion de Redes",
+    },
+  ];
+
+  const informacion = [
+    {
+      titulo: "Dominios",
+    },
+    {
+      titulo: "Hosting",
+    },
+    {
+      titulo: "Precios",
+    },
+    {
+      titulo: "Formas de Pago",
+    },
+  ];
+
+  const compania = [
+    {
+      titulo: "Sobre Hexágono",
+    },
+    {
+      titulo: "Tecnologías que utilizamos",
+    },
+    {
+      titulo: "Mapa de Ruta",
+    },
+    {
+      titulo: "Blog",
+    },
+  ];
+
   const redes = [
     {
       titulo: "Diseño Responsivo",
@@ -40,60 +85,48 @@ const Footer = () => {
             <span className="flex justify-center text-amber-50">2020-2024</span>
           </p>
         </figure>
+
         <section className="col-span-1 row-span-4 flex flex-col lg:justify-start lg:items-start mb-10">
-          <ul className=" text-center text-[var(--button-pres)] text-sm font-extralight">
+          <ul className="text-center lg:text-left text-[var(--button-pres)] text-sm font-extralight">
             <h1 className="text-2xl text-amber-100 mb-5 lg:mt-32">
               Navegación
             </h1>
-            <li className="hover:text-[var(--font-semi-light)]">
-              Desarrollo Web
-            </li>
-            <li className="hover:text-[var(--font-semi-light)]">
-              Diseño gráfico
-            </li>
-            <li className="hover:text-[var(--font-semi-light)]">Modelado 3D</li>
-            <li className="hover:text-[var(--font-semi-light)]">
-              Difusion de Redes
-            </li>
+            {navegacion.map(({ titulo }) => (
+              <li className="hover:text-[var(--font-light)]">{titulo}</li>
+            ))}
           </ul>
         </section>
+
         <section className="col-span-1 row-span-4 flex flex-col lg:justify-start lg:items-start mb-10">
-          <ul className=" text-center text-[var(--button-pres)] text-sm font-extralight">
+          <ul className="text-center lg:text-left text-[var(--button-pres)] text-sm font-extralight">
             <h1 className="text-2xl text-amber-100 mb-5 lg:mt-32">
               Información
             </h1>
-            <li className="hover:text-[var(--font-semi-light)]">Dominios</li>
-            <li className="hover:text-[var(--font-semi-light)]">Hosting</li>
-            <li className="hover:text-[var(--font-semi-light)]">Precios</li>
-            <li className="hover:text-[var(--font-semi-light)]">
-              Formas de pago
-            </li>
+            {informacion.map(({ titulo }) => (
+              <li className="hover:text-[var(--font-light)]">{titulo}</li>
+            ))}
           </ul>
         </section>
+
         <section className="col-span-1 row-span-4 flex flex-col lg:justify-start lg:items-start mb-10">
-          <ul className=" text-center text-[var(--button-pres)] text-sm font-extralight">
-            <h1 className="text-2xl text-amber-100 mb-5 lg:mt-32">Compañía</h1>
-            <li className="hover:text-[var(--font-semi-light)]">
-              Sobre Hexágono
-            </li>
-            <li className="hover:text-[var(--font-semi-light)]">
-              Tecnologías que utilizamos
-            </li>
-            <li className="hover:text-[var(--font-semi-light)]">
-              Mapa de ruta
-            </li>
-            <li className="hover:text-[var(--font-semi-light)]">Blog</li>
+          <ul className="text-center lg:text-left text-[var(--button-pres)] text-sm font-extralight">
+            <h1 className="text-2xl text-amber-100 mb-5 lg:mt-32">Compañía
+            </h1>
+            {compania.map(({ titulo }) => (
+              <li className="hover:text-[var(--font-light)]">{titulo}</li>
+            ))}
           </ul>
         </section>
+
         <section className="col-span-1 row-span-4 flex flex-col lg:justify-start lg:items-start mb-10">
-          <ul className=" text-center text-[var(--button-pres)] text-sm font-extralight">
+          <ul className="text-center lg:text-left text-[var(--button-pres)] text-sm font-extralight">
             <h1 className="text-2xl text-amber-100 mb-5 lg:mt-32">Redes</h1>
-            <li className="hover:text-[var(--font-semi-light)]">Facebook</li>
-            <li className="hover:text-[var(--font-semi-light)]">Instagram</li>
-            <li className="hover:text-[var(--font-semi-light)]">WhatsApp</li>
-            <li className="hover:text-[var(--font-semi-light)]">Youtube</li>
+            {redes.map(({ titulo }) => (
+              <li className="hover:text-[var(--font-light)]">{titulo}</li>
+            ))}
           </ul>
         </section>
+
         <section className="col-span-6 flex justify-center items-center my-10 text-[var(--font-light)]">
           <p>
             Página creada con <span className="text-amber-100">⌨</span> por Bird
