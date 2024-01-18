f4736cbb76074682ea43253a6860f36022e61773
gramaphenia <dicoratojuanpablo@gmail.com>
2024-01-18 19:56:15 -0300

    add-link

--- src/components/ListaModalMenu.jsx
+++ src/components/ListaModalMenu.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 // Constantes de estilos
 const listItemStyle =
@@ -8,9 +9,15 @@ const ModalList = ({ title, items }) => (
   <section className="col-span-1 row-span-4 flex flex-col lg:justify-start lg:items-center mb-10">
     <ul className="text-center lg:text-left">
       <h1 className="text-2xl text-amber-100 mb-5 ">{title}</h1>
-      {items.map(({ titulo }) => (
+      {items.map(({ titulo, enlace }) => (
         <li key={titulo} className={listItemStyle}>
-          {titulo}
+          {/* Verifica si hay un enlace antes de renderizar el componente Link */}
+          {enlace ? (
+            <Link target="_blank" href={enlace}>{titulo}</Link>
+          ) : (
+            // Si no hay enlace, renderiza solo el título
+            <span>{titulo}</span>
+          )}
         </li>
       ))}
     </ul>
@@ -19,31 +26,31 @@ const ModalList = ({ title, items }) => (
 
 const ListaModalMenu = () => {
   const navegacion = [
-    { titulo: "Desarrollo Web" },
-    { titulo: "Diseño Gráfico" },
-    { titulo: "Modelado 3D" },
-    { titulo: "Difusion de Redes" },
+    { titulo: "Desarrollo Web", enlace: "/desarrollo-web" },
+    { titulo: "Diseño Gráfico", enlace: "/diseno-grafico" },
+    { titulo: "Modelado 3D", enlace: "/modelado-3d" },
+    { titulo: "Difusión de Redes", enlace: "/difusion-redes" },
   ];
 
   const informacion = [
-    { titulo: "Dominios" },
-    { titulo: "Hosting" },
-    { titulo: "Precios" },
-    { titulo: "Formas de Pago" },
+    { titulo: "Dominios", enlace: "/dominios" },
+    { titulo: "Hosting", enlace: "/hosting" },
+    { titulo: "Precios", enlace: "/precios" },
+    { titulo: "Formas de Pago", enlace: "/formas-pago" },
   ];
 
   const compania = [
-    { titulo: "Sobre Hexágono" },
-    { titulo: "Tecnologías que utilizamos" },
-    { titulo: "Mapa de Ruta" },
-    { titulo: "Blog" },
+    { titulo: "Sobre Hexágono", enlace: "/sobre-hexagono" },
+    { titulo: "Tecnologías que utilizamos", enlace: "/tecnologias" },
+    { titulo: "Mapa de Ruta", enlace: "/mapa-ruta" },
+    { titulo: "Blog", enlace: "/blog" },
   ];
 
   const redes = [
-    { titulo: "Instagram" },
-    { titulo: "Facebook" },
-    { titulo: "Youtube" },
-    { titulo: "WhatsApp" },
+    { titulo: "Instagram", enlace: "https://www.instagram.com/" },
+    { titulo: "Facebook", enlace: "https://www.facebook.com/" },
+    { titulo: "Youtube", enlace: "https://www.youtube.com/" },
+    { titulo: "WhatsApp", enlace: "https://wa.me/" },
   ];
 
   return (
