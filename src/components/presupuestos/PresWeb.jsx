3a6d80fa243e930fca65f425f8a6e8f35f1923ec
gramaphenia <dicoratojuanpablo@gmail.com>
2024-02-18 06:54:28 -0300

    responsive-planes

--- src/components/presupuestos/PresWeb.jsx
+++ src/components/presupuestos/PresWeb.jsx
@@ -28,10 +28,10 @@ const PresWeb = () => {
   return (
     <>
       <form
-        className="justify-center items-center border border-[var(--button-dark)] py-10 lg:p-10 rounded-xl bg-[#1011121d] backdrop-blur-sm mx-auto w-[340px] lg:w-[700px] sombra-inactiva grid grid-cols-1 lg:grid-cols-2 gap-4"
+        className="justify-center items-center border border-[var(--button-dark)] py-10 p-10 lg:p-10 rounded-xl bg-[#1011121d] backdrop-blur-sm mx-auto w-[340px] lg:w-[700px] sombra-inactiva grid grid-cols-1 lg:grid-cols-2 gap-4"
         onSubmit={handleSubmit}
       >
-        <div className="flex flex-col col-span-2 text-sm mb-1 text-amber-100 font-extralight">
+        <div className="col-span-2 text-sm mb-1 text-amber-100 font-extralight">
           <label
             className="text-sm mb-1 text-amber-100 font-extralight"
             htmlFor="servicio"
@@ -39,7 +39,7 @@ const PresWeb = () => {
             Servicio:
           </label>
           <select
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-amber-200/ block w-full p-2.5 dark:bg-gray-800 dark:border-amber-100 dark:placeholder-gray-700 dark:text-white dark:focus:ring-amber-200 dark:focus:border-amber-200 mb-2"
+            className="col-span-2 lg:col-span-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-amber-200/ block w-full p-2.5 dark:bg-gray-800 dark:border-amber-100 dark:placeholder-gray-700 dark:text-white dark:focus:ring-amber-200 dark:focus:border-amber-200 mb-2"
             id="servicio"
             value={servicio}
             onChange={(e) => setServicio(e.target.value)}
@@ -53,7 +53,7 @@ const PresWeb = () => {
             <option value="Plan Empresarial">Plan Empresarial</option>
           </select>
         </div>
-        <div className="flex flex-col">
+        <div className="col-span-2 lg:col-span-1 flex flex-col">
           <label
             className="text-sm mb-1 text-amber-100 font-extralight"
             htmlFor="calidadDiseno"
@@ -68,7 +68,7 @@ const PresWeb = () => {
             onChange={(e) => setCalidadDiseno(e.target.value)}
           />
         </div>
-        <div className="flex flex-col">
+        <div className="col-span-2 lg:col-span-1  flex flex-col">
           <label
             className="text-sm mb-1 text-amber-100 font-extralight"
             htmlFor="calidadDesarrollo"
@@ -83,7 +83,7 @@ const PresWeb = () => {
             onChange={(e) => setCalidadDesarrollo(e.target.value)}
           />
         </div>
-        <div className="flex flex-col">
+        <div className="col-span-2 lg:col-span-1 flex flex-col">
           <label
             className="text-sm mb-1 text-amber-100 font-extralight"
             htmlFor="calidadModelado"
@@ -98,7 +98,7 @@ const PresWeb = () => {
             onChange={(e) => setCalidadModelado(e.target.value)}
           />
         </div>
-        <div className="flex flex-col text-sm mb-1 text-amber-100 font-extralight">
+        <div className="col-span-2 lg:col-span-1 flex flex-col text-sm mb-1 text-amber-100 font-extralight">
           <label htmlFor="nombre">Nombre:</label>
           <input
             className="border border-[var(--button-dark)] rounded-sm bg-transparent p-1 text-[var(--font-light)] font-extralight focus:outline-none"
@@ -108,7 +108,7 @@ const PresWeb = () => {
             onChange={(e) => setNombre(e.target.value)}
           />
         </div>
-        <div className="flex flex-col text-sm mb-1 text-amber-100 font-extralight">
+        <div className="col-span-2 lg:col-span-1 flex flex-col text-sm mb-1 text-amber-100 font-extralight">
           <label htmlFor="email">Email:</label>
           <input
             className="border border-[var(--button-dark)] rounded-sm bg-transparent p-1 text-[var(--font-light)] font-extralight focus:outline-none"
@@ -118,7 +118,7 @@ const PresWeb = () => {
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <div className="flex flex-col text-sm mb-1 text-amber-100 font-extralight">
+        <div className="col-span-2 lg:col-span-1 flex flex-col text-sm mb-1 text-amber-100 font-extralight">
           <label htmlFor="telefono">Teléfono:</label>
           <input
             className="border border-[var(--button-dark)] rounded-sm bg-transparent p-1 text-[var(--font-light)] font-extralight focus:outline-none"
@@ -129,7 +129,7 @@ const PresWeb = () => {
           />
         </div>
         <button
-          className="w-[250px] col-span-2 flex justify-center items-center text-[var(--font-light)] bg-amber-200/90 px-8 py-4 rounded-full sombra-light-amber border border-slate-600/40 m-10 mx-auto text-slate-800"
+          className="w-[250px] col-span-2 flex justify-center items-center  bg-amber-200/90 hover:bg-amber-200 px-8 py-4 rounded-full sombra-light-amber border border-slate-600/40 m-10 mx-auto text-slate-800 hover:text-[#171c21]"
           type="submit"
         >
           Pedir ahora
